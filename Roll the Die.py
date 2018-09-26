5bdff979981faaacef985bf77bfde515e19c9531
Seulmin (Andy) Ryu <seulmin.ryu@nyu.edu>
2018-09-26 00:21:36 -0400

    Update Roll the Die.py

--- Roll the Die.py
+++ Roll the Die.py
@@ -16,7 +16,6 @@ while True:
     if sides <= 3 or sides >= 20:
         print("Sides must be between 3 and 20. Try again.")
         sides = int(input("How many sides to your die (3-20)?"))
-
     elif 3 < sides < 20:
         break
 
@@ -26,7 +25,6 @@ while True:
     if rolls <= 0:
         print("Can't roll", rolls, "times.", "Try again.")
         rolls = int(input("How many rolls do you want?"))
-
     elif rolls > 0:
         print()
         print("Here we go!")
@@ -61,14 +59,10 @@ else:
 
 for j in range(1, sides+1):
     if max(countlist) > 50:
-        
         numstar = ((countlist[j-1])//increment)
-        
         numspace = len(str(countlist[j-1]))
         space = str(numspace)+"d"
-        
         print("Side", format(j,'2d'), ":", format(countlist[j-1], ">" + str(space)), "times:", "*"*numstar)
-    
     else:
         print("Side", format(j,'2d'), ":", format(countlist[j-1],'2d'), "times:", "*"*countlist[j-1])
 
